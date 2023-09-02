6794ebb1e0d3022a336b6259583fd1f5dee9195d
Sam Zanca <samuele@zanca.dev>
2023-09-02 14:05:05 -0400

    lots of restructuring and some base styles

---
+++ src/components/index.ts
@@ -0,0 +1,8 @@
+// Core
+export {default as Html} from './Html.astro'
+export {default as Navigation} from './Navigation.astro'
+export {default as Head} from './Head.astro'
+export {default as Footer} from './Footer.astro'
+
+// Utils
+export {default as FormattedDate} from './FormattedDate.astro'
---
+++ src/layouts/index.ts
@@ -0,0 +1,2 @@
+export {default as Page} from './Page.astro'
+export {default as BlogPost} from './BlogPost.astro'
